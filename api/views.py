8828b73253bd34b6d8d203bfa5d29bfd98683fd1
Tyler Galvin <tylergalvin97@gmail.com>
2022-04-06 18:25:45 -0400

    permission

--- api/views.py
+++ api/views.py
@@ -1,15 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Card, User
 from .serializer import CardSerializer, UserSerializer 
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView 
 
 class UserListView(generics.ListCreateAPIView):
-  queryset = User.objects.all()
-  serializer_class = UserSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class CardListView(generics.ListCreateAPIView):
-  queryset = Card.objects.all()
-  serializer_class = CardSerializer
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+    permissions = (permissions.IsAuthenticatedOrReadOnly)
 
