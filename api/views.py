56f1e459f0c42b08e0ec581f8c8ae964236bde72
Tyler Galvin <tylergalvin97@gmail.com>
2022-04-12 15:16:04 -0400

    stuffff

--- api/views.py
+++ api/views.py
@@ -56,7 +56,7 @@ class UserCreatedCardListView(generics.ListCreateAPIView):
 class CardDetailsView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsOwnerOrReadOnly,)
 
 
 class UserDetailsView(generics.RetrieveUpdateDestroyAPIView):
